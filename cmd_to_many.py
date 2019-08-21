029235fd3b9a4fd6704e0c7379be354e64f5b326
mwit <20845116+mattwitmer@users.noreply.github.com>
2019-08-21 14:28:27 -0500

    Add files via upload

--- cmd_to_many.py
+++ cmd_to_many.py
@@ -6,8 +6,26 @@ from __future__ import print_function, unicode_literals
 from netmiko import ConnectHandler
 from datetime import datetime
 import getpass
+import sys
 
-# function to prompt for user command input and issue command
+file_time = datetime.now()
+datetimeStr = file_time.strftime("%Y%m%d-%H%M%S")
+
+# class to print output to console and capture to file
+class Logger(object):
+
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open(datetimeStr + "_output.txt", "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        pass 
+
+# login function to read hostfile and input prompt for user credentials
 def login():
 
     # open host file and read to string
@@ -15,21 +33,21 @@ def login():
         ipaddr = f.read().splitlines()
     f.close()
 
-    #uname = getpass.getuser() #uncomment to read username from Windows...commented out for GNS3 testing
-    uname = input('username:') #prompt for username
-    passwd = getpass.getpass(prompt='Password: ', stream=None) #prompt for pw
+    #uname = getpass.getuser() #get sername from Windows credentials
+    uname = input('username:') #uncomment to prompt for user input
+    passwd = getpass.getpass(prompt='Password: ', stream=None)
 
     return ipaddr,uname,passwd
 
-# function to input and issue command
+# function to prompt for user command input and issue command
 def cmd():
-
-    # open capture output file
-    file = open('output.txt', 'w')
 
     hostfile,uname,passwd = login()
     
     command = input("Enter Global Config Command:")
+
+    # call Logger class to print output to console and file
+    sys.stdout = Logger()
 
     # iterate through hostfile for ssh and sending command
     for ipaddr in hostfile:
@@ -48,22 +66,21 @@ def cmd():
             continue
 
         output = net_connect.send_command(command)
-
+        
         print("\n")
         print("#" * 80)
+        print(ipaddr)
+        print("\n")
         print(output)
         print("#" * 80) 
         print("\n")
 
-        file.write(output)
-
-    file.close()
-
-# main starts script run timer
+    
+# main prints script run time
 def main():
 
     start_time = datetime.now()
-
+    
     cmd()
 
     end_time = datetime.now()
